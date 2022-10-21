b075f041686c6e3e1604615bc0de143ab08abc51
basill099 <iamblackgold99@yahoo.com>
2022-10-21 06:46:17 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,9 +2,9 @@ include "main.h"
 
 
 /**
- * _abs-prints the absolute value of an integer.
- * @a:the integer to compute its absolute value.
- * Return:returns the abs value.
+ * _abs - computes  the absolute value of an integer.
+ * @a: the integer to compute its absolute value.
+ * Return: returns the abs value.
  **/
 
 int _abs(int a)
