4087b889e96cc4eb694b2d12c5a89bc29eda8329
basill099 <iamblackgold99@yahoo.com>
2022-10-27 19:11:03 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * string_toupper - changes all lowercase letters of a string
- * to uppercase
+ * string_toupper - changes all lowercase letters of a string to uppercase
  * @s: input string.
  * Return: the pointer to dest.
  **/
